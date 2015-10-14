c1e49b9a8d73a4167ad7b62d1ae3a792b0bf99db
Marcus Battle <marcusbattle@Marcbook.local>
2015-10-14 14:06:47 -0400

    Updated name lable for logged in user

--- parts/loop-home.php
+++ parts/loop-home.php
@@ -1,6 +1,16 @@
 <div class="status">
 	<div class="header">
-		<span class="author"><?php echo the_author_meta( 'display_name' ); ?></span> <span class="time-published"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></span>
+		<span class="avatar"></span>
+		<span class="author">
+			<?php 
+				if ( get_the_author_id() == get_current_user_id() ) { 
+					echo _e( "Me" );
+				} else {
+					echo the_author_meta( 'display_name' );
+				} 
+			?>
+		</span> 
+		<span class="time-published"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></span>
 	</div>
 	<?php echo the_content(); ?>
 </div>
