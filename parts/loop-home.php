2d071f281f9978dd887e3baf3e2ac1ad3cb1f704
Marcus Battle <marcusbattle@Marcbook.local>
2015-10-14 02:34:12 -0400

    Added status header

--- parts/loop-home.php
+++ parts/loop-home.php
@@ -1,3 +1,6 @@
 <div class="status">
+	<div class="header">
+		<span class="author"><?php echo the_author_meta( 'display_name' ); ?></span> <span class="time-published"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></span>
+	</div>
 	<?php echo the_content(); ?>
 </div>
