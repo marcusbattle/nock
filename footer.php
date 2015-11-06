6ec5fe9035f4c2406c82f43d47c9051a3f1cfbc3
Marcus Battle <marcusbattle@Marcbook.local>
2015-11-06 13:49:20 -0500

    Renamed Feed to Tags

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="nav-bar">
 	<div class="btn-group btn-group-justified" role="group">
-		<a class="btn btn-default" href="<?php echo home_url(); ?>">Feed</a>
+		<a class="btn btn-default" href="<?php echo home_url(); ?>">Tags</a>
 		<a class="btn btn-default" href="groups">Groups</a>
 		<a class="btn btn-default">Profile</a>
 	</div>
